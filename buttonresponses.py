4494c769a64f25980de0e8c2f8604b3fd72ed667
RoyT2 <112135315+Dogutak@users.noreply.github.com>
2022-09-13 21:40:27 +0000

    Added The first question - the input from each are yet to come

---
+++ buttonresponses.py
@@ -0,0 +1,19 @@
+#Question 1
+#<------------------------>
+def fng():
+    print("a")
+
+
+def sns():
+    print("a")
+
+
+def qns():
+    print("a")
+
+
+def snc():
+    print("a")
+
+
+#<------------------------>
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import buttonresponses
 from PIL import ImageTk, Image
 
 #TEMPLATE CODE
@@ -16,51 +17,65 @@ menuwindow.geometry("835x635")
 #This is the menu screen canvas
 menuplatform = tk.Canvas(menuwindow, width=835, height=635)
 #This is the info page canvas
-infopageplatform = tk.Canvas(menuwindow, width=835, height = 635)
+infopageplatform = tk.Canvas(menuwindow, width=835, height=635)
+#<---------->
 
 #Under this is all the button canvases
 #<---------->
 #This is the BEGIN button canvas
-beginbuttonplatform= tk.Canvas(menuwindow, width=50, height=50)
+beginbuttonplatform = tk.Canvas(menuwindow, width=50, height=50)
 #This is the Yesbutton canvas
-yesbuttonplatform= tk.Canvas(menuwindow, width=50, height=50)
+yesbuttonplatform = tk.Canvas(menuwindow, width=50, height=50)
 #This is the No button canvas
-nobuttonplatform= tk.Canvas(menuwindow, width=50, height=50)
+nobuttonplatform = tk.Canvas(menuwindow, width=50, height=50)
+
+#<---------->
+
 
 #ALL THE FUNCTIONS ARE UNDER HERE
 #<------------------------>
 def disclaimer():
-  begin_button.destroy()
-  menuplatform.create_image(415, 315, image=infopage_img)
-  yesbtn.place(x=217, y=350)
-  nobtn.place(x=487, y=350)
+    begin_button.destroy()
+    menuplatform.create_image(415, 315, image=infopage_img)
+    yesbtn.place(x=217, y=350)
+    nobtn.place(x=487, y=350)
+
+
 def firstques():
-  yesbtn.destroy()
-  nobtn.destroy()
-  menuplatform.create_image(415, 315, image=firstques_img)
+    yesbtn.destroy()
+    nobtn.destroy()
+    menuplatform.create_image(415, 315, image=firstques_img)
+    btnsq1()
 
 
+def btnsq1():
+    fngbtn.place(x=217, y=250)
+    snsbtn.place(x=487, y=250)
+    qnsbtn.place(x=217, y=450)
+    sncbtn.place(x=487, y=450)
 
-  
+
 def closequiz():
-  menuwindow.destroy()
-#<------------------------>
+    menuwindow.destroy()
 
+
+#<------------------------>
 
 #FIRST QUESTION CODE
 #<------------------------>
-firstques_img = ImageTk.PhotoImage(file="question1.jpeg") #Directory file finder
-
+firstques_img = ImageTk.PhotoImage(
+    file="imagescreens/question1.png")  #Directory file finder
 
 #<------------------------>
 
 #MENU SCREEN IMAGE CODE
 #<--------------------->
-valorant_img = ImageTk.PhotoImage(file="val.png") #Directory file finder
+valorant_img = ImageTk.PhotoImage(
+    file="imagescreens/val.png")  #Directory file finder
 #This is to actually show the canvas
 menuplatform.pack()
 #This is the code to call the image
-menu = ImageTk.PhotoImage(Image.open("val.png"))
+menu = ImageTk.PhotoImage(Image.open("imagescreens/val.png"))
 menuimage = tk.Label(menuwindow, image=menu)
 #This is to actually show the image
 menuimage.pack()
@@ -70,25 +85,49 @@ menuplatform.create_image(415, 315, image=valorant_img)
 
 #YES AND NO BUTTON CODE
 #<-------------------->
-yesbtn = tk.Button(text="Yes", command = firstques)
+yesbtn = tk.Button(text="Yes", command=firstques)
 yesbtn.configure(width=10)
-nobtn = tk.Button(text="No", command = closequiz)
+nobtn = tk.Button(text="No", command=closequiz)
 nobtn.configure(width=10)
 #<-------------------->
 
 #BEGIN BUTTON CODE
 #<------------------------>
 #This finds the file for the image of the "BEGIN" button.
-begin_image = ImageTk.PhotoImage(Image.open("button.png")) #^Directory file finder
+begin_image = ImageTk.PhotoImage(
+    Image.open("imagescreens/button.png"))  #^Directory file finder
 #This creates the area for the BEGIN button to actually work.
-begin_button = tk.Button(text="Start the Test", image=begin_image, command = disclaimer)
+begin_button = tk.Button(text="Start the Test",
+                         image=begin_image,
+                         command=disclaimer)
 begin_button.configure(width=227)
 begin_button_window = beginbuttonplatform.create_window(30, 40)
 #This places the "BEGIN" button where I want it to be - note the coordinates.
-infopage_img = ImageTk.PhotoImage(file="info page.png") #Directory file finder
+infopage_img = ImageTk.PhotoImage(
+    file="imagescreens/info page.png")  #Directory file finder
 
 begin_button.place(x=287, y=350)
 #<------------------------>
 
-
 print("Running")
+#QUESTION 1 CODE
+#<------------------------>
+
+#BUTTON CODE
+#<------------------------>
+
+#ANSWER 1 "Fast and Aggressive"
+fngbtn = tk.Button(text="Fast and Aggressive", command=buttonresponses.fng)
+
+#ANSWER 2 "Slow and Steady"
+snsbtn = tk.Button(text="Slow and Steady", command=buttonresponses.sns)
+
+#ANSWER 3 "Quiet and Stealthy"
+qnsbtn = tk.Button(text="Quiet and Stealthy", command=buttonresponses.qns)
+
+#ANSWER 4 "Safe and Cautious"
+sncbtn = tk.Button(text="Safe and Cautious", command=buttonresponses.snc)
+
+#<------------------------>
+
+#<------------------------>
